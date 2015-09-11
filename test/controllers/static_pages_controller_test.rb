f5dc85763822c17889f1c05a725b15cc72687fad
tianyaqu <tianyaqu@qq.com>
2015-09-11 15:49:03 +0800

    use provide and yield in tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @site = "MicroBlog"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home | #{@site}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help | #{@site}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title","About | #{@site}"
   end
 end
